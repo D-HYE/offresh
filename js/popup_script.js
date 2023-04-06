afd8df78dc585a2a56d84da66e49da648dfdeef1
dahye <pag9612@gmail.com>
2023-04-06 16:07:47 +0900

    first commit 0406

--- js/popup_script.js
+++ js/popup_script.js
@@ -1,4 +1,64 @@
+$('.radio_list_wrap .radio_list .img_box').click(function(){
+    let soldOut_check = $(this).closest('.radio_list').hasClass('sold_out');
+    let checkde_radio2 = $(this).find('input:radio').is(':checked');
+    if(soldOut_check !== true){
+        if(checkde_radio2 !== true){
+            $(this).closest('.radio_list_wrap').find('.radio_list').removeClass('checked');
+            $(this).closest('.radio_list').addClass('checked');
 
+            $(this).closest('.radio_list_wrap').find('.radio_box_wrap').removeClass('checked');
+            $(this).find('.radio_box_wrap').addClass('checked');
+
+            $(this).closest('.radio_list_wrap').find('input:radio').attr('checked', false);
+            $(this).closest('.radio_list').find('input:radio').attr('checked', true)
+        }else{
+            $(this).closest('.radio_list_wrap').find('.radio_list').removeClass('checked');
+            $(this).closest('.radio_list_wrap').find('.radio_box_wrap').removeClass('checked');
+            $(this).closest('.radio_list_wrap').find('input:radio').attr('checked', false);
+        }
+    }
+});
+
+$('ol .radio_box_wrap').click(function(){
+    let checkde_radio1 = $(this).find('input:radio').is(':checked');
+    if(checkde_radio1 !== true){
+        $(this).parents().find('.radio_box_wrap').removeClass('checked');
+        $(this).addClass('checked');
+        $(this).parents().find('input:radio').attr('checked', false);
+        $(this).find('input:radio').attr('checked', true)
+    };
+});
+
+$('.privacy_policy_area .check_box_wrap').click(function(){
+    let checkde_checkbox1 = $(this).find('input:checkbox').is(':checked');
+    if(checkde_checkbox1 !== false){
+        $(this).closest('.privacy_policy_area').addClass('open');
+    }else{
+        $(this).closest('.privacy_policy_area').removeClass('open')
+    }
+});
+
+$('.privacy_policy_area .agreement_btn').click(function(){
+    let has_open = $(this).closest('.privacy_policy_area').hasClass('open')
+    if(has_open !== true){
+        $(this).closest('.privacy_policy_area').addClass('open');
+    }else{
+        $(this).closest('.privacy_policy_area').removeClass('open')
+    }
+});
+
+$('.inquiry_case_list li').click(function(){
+    $(this).closest('.inquiry_case_list').find('li').removeClass('on');
+    $(this).addClass('on');
+});
+
+$('.inquiry_btn_wrap.single_choice li').click(function(){
+    $(this).closest('.inquiry_btn_wrap').find('li').removeClass('checked');
+    $(this).addClass('checked');
+});
+$('.inquiry_btn_wrap.multiple_choice li').click(function(){
+    $(this).toggleClass('checked');
+});
 
 // ------ 서비스 문의, 메인 창
 $('#inquiry_case_btn1').on('click', function () {
